3d090db8fe62b0ab8d0597d4c6336819ee91f566
Damien Pontifex <damien.pontifex@gmail.com>
2020-07-10 18:30:33 +0800

    connecting to wifi

--- main/main.c
+++ main/main.c
@@ -2,9 +2,18 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <dht.h>
+#include <esp_wifi.h>
+#include <esp_log.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
 
 static const dht_sensor_type_t sensor_type = DHT_TYPE_AM2301;
 static const gpio_num_t dht_gpio = 17;
+
+#define EXAMPLE_WIFI_SSID CONFIG_WIFI_SSID
+#define EXAMPLE_WIFI_PASS CONFIG_WIFI_PASSWORD
+
+static const char *TAG = "power_save";
 
 void dht_test(void *pvParameters)
 {
@@ -23,7 +32,56 @@ void dht_test(void *pvParameters)
   }
 }
 
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        esp_wifi_connect();
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip: " IPSTR, IP2STR(&event->ip_info.ip));
+    }
+}
+
+static void initialise_wifi(void)
+{
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+  assert(sta_netif);
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, NULL));
+
+  wifi_config_t wifi_config = {
+      .sta = {
+          .ssid = EXAMPLE_WIFI_SSID,
+          .password = EXAMPLE_WIFI_PASS,
+          .listen_interval = 3,
+      },
+  };
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+
+  ESP_LOGI(TAG, "esp_wifi_set_ps().");
+  esp_wifi_set_ps(WIFI_PS_NONE);
+}
+
 void app_main()
 {
-  xTaskCreate(dht_test, "dht_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
+  // Initialise NVS
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+  // xTaskCreate(dht_test, "dht_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
+  initialise_wifi();
 }
